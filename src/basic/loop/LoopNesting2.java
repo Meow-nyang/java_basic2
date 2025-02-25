fc57fc2e688ff69e9910ce14430bde67509663db
LeeKM321 <stephen4951@gmail.com>
2025-02-25 17:49:17 +0900

    Day03 - 반복문 중첩

---
+++ src/basic/loop/LoopNesting2.java
@@ -0,0 +1,77 @@
+package basic.loop;
+
+public class LoopNesting2 {
+
+    public static void main(String[] args) {
+
+        /*
+
+         *
+         **
+         ***
+         ****
+         *****
+
+         */
+        for (int i=1; i<=5; i++) {
+            for (int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        /*
+             *
+            **
+           ***
+          ****
+         *****
+         */
+
+        for (int i=1; i<=5; i++) {
+            // 별 찍기 전에 공백부터 만드는 for문
+            for (int k=4; k >= i; k--) {
+                System.out.print(" ");
+            }
+
+            // 별찍는 for문
+            for (int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        /*
+        *****
+         ****
+          ***
+           **
+            *
+         */
+        for (int i=1; i<=5; i++) {
+            // 별 찍기 전에 공백부터 만드는 for문
+            for (int k=0; k < i-1; k++) {
+                System.out.print(" ");
+            }
+
+            // 별찍는 for문
+            for (int j=5; j>=i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
