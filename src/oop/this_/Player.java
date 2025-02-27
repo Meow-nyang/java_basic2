c1f9f73554ca6bd6c306dab89fd3b730b110f749
LeeKM321 <stephen4951@gmail.com>
2025-02-27 17:57:08 +0900

    Day05 - this 마무리

--- src/oop/this_/Player.java
+++ src/oop/this_/Player.java
@@ -9,25 +9,49 @@ public class Player {
 
     Player() {
         System.out.println("Player의 기본 생성자 호출!");
-        level = 1;
-        hp = 50;
+        this.level = 1;
+        this.hp = 50;
     }
 
     Player(String nickName) {
+        this();
         System.out.println("Player의 2번 생성자 호출!");
-        System.out.println("this가 띠는 주소값: " + this);
         this.nickName = nickName;
-        this.level = 1;
-        hp = 50;
+    }
+
+    Player(String nickName, int hp) {
+        this(nickName);
+        System.out.println("Player의 3번 생성자 호출!");
+        this.hp = hp;
+    }
+
+    // 일반 메서드
+    void attack(Player target) {
+        if (target == this) {
+            System.out.println("타겟이 잘못되었습니다.");
+            return;
+        }
+
+        System.out.printf("%s이(가) %s을(를) 공격합니다!\n"
+                , this.nickName, target.nickName);
+
+        // 맞는 Player의 체력을 10 감소시키자.
+        target.hp -= 10;
+
+        // 때리는 Player의 체력은 3 감소시키자.
+        this.hp -= 3;
+
+        System.out.printf("%s님의 남은 체력: %d\n", this.nickName, this.hp);
+        System.out.printf("%s님의 남은 체력: %d\n", target.nickName, target.hp);
     }
 
 
-//    @Override
-//    public String toString() {
-//        return "Player{" +
-//                "nickName='" + nickName + '\'' +
-//                ", level=" + level +
-//                ", hp=" + hp +
-//                '}';
-//    }
+    @Override
+    public String toString() {
+        return "Player{" +
+                "nickName='" + nickName + '\'' +
+                ", level=" + level +
+                ", hp=" + hp +
+                '}';
+    }
 }
