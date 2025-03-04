563fab6e98f331f2c22b24af9993065a3435059c
LeeKM321 <stephen4951@gmail.com>
2025-03-04 10:14:14 +0900

    Day07 - 연습문제 풀이(encap, inherit, static)

---
+++ src/oop/static_/practice/Product.java
@@ -0,0 +1,17 @@
+package oop.static_.practice;
+
+public class Product {
+
+    String productName;
+    int price;
+
+    public Product(String productName, int price) {
+        this.productName = productName;
+        this.price = price;
+    }
+
+    public void sellProduct() {
+        Store.addSale(this.price);
+    }
+
+}
