127c3274c339a2c169a7d6edf5acb0796b17ce0c
Meow_nyang <slah76@naver.com>
2025-03-17 18:13:07 +0900

    Day09 - 제네릭

---
+++ src/etc/generic/good/MultiBox.java
@@ -0,0 +1,26 @@
+package etc.generic.good;
+
+public class MultiBox<K, V> {
+
+    private K key;
+    private V value;
+
+    public MultiBox() {}
+
+    public MultiBox(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public V get() {
+        return value;
+    }
+
+    public void put(K key, V value) {
+        this.key = key;
+        this.value = value;
+
+
+    }
+
+}
